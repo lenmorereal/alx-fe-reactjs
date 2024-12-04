2ef962bd0dd84c5e2d34e00aed7f1abb9a8b10dd
Your Name <your_email@example.com>
2024-12-04 07:51:09 +0200

    Move UserContext.js to src/Context directory

---
+++ alx-react-app-props/src/Context/UserContext.js
@@ -0,0 +1,16 @@
+// src/context/UserContext.js
+import React, { createContext, useState } from 'react';
+
+export const UserContext = createContext();
+
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({ name: "John Doe", email: "johndoe@example.com" });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
