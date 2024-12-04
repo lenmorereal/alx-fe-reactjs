221d28d2010dad932a6b1e492e739a7fa37d7a54
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-04 07:32:49 +0200

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,7 +1,16 @@
-import { createContext } from 'react';
+// src/context/UserContext.js
+import React, { createContext, useState } from 'react';
 
-// Create the context
-const UserContext = createContext();
+export const UserContext = createContext();
 
-// Export the context
-export default UserContext;
+const UserProvider = ({ children }) => {
+  const [user, setUser] = useState({ name: "John Doe", email: "johndoe@example.com" });
+
+  return (
+    <UserContext.Provider value={{ user, setUser }}>
+      {children}
+    </UserContext.Provider>
+  );
+};
+
+export default UserProvider;
