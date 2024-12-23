5ae1d7fddbc491d2375ba2c76475f60ee9951cac
Your Name <your_email@example.com>
2024-12-23 10:25:53 +0200

    Implemented form handling with controlled components and Formik

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import RegistrationForm from './components/RegistrationForm';
+import FormikForm from './components/FormikForm';
+
+function App() {
+  return (
+    <div>
+      <h1>User Registration</h1>
+      <FormikForm />
+    </div>
+  );
+}
+
+export default App;
