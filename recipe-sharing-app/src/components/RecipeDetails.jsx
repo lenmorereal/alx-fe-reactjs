fbdc6baab016597d4689e0993d9da2cf2c6c260f
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-25 00:05:54 +0200

    Update RecipeDetails.jsx

--- recipe-sharing-app/src/components/RecipeDetails.jsx
+++ recipe-sharing-app/src/components/RecipeDetails.jsx
@@ -1,26 +1,18 @@
-import { useRecipeStore } from '../store/recipeStore';
+import React from 'react';
 import { useParams } from 'react-router-dom';
-import EditRecipeForm from './EditRecipeForm';
-import DeleteRecipeButton from './DeleteRecipeButton';
+import { useRecipeStore } from './recipeStore';
 
 const RecipeDetails = () => {
   const { id } = useParams();
-  const recipe = useRecipeStore((state) =>
-    state.recipes.find((recipe) => recipe.id === parseInt(id))
-  );
+  const recipe = useRecipeStore(state => state.recipes.find(r => r.id === Number(id)));
+
+  if (!recipe) return <div>Recipe not found</div>;
 
   return (
     <div>
-      {recipe ? (
-        <>
-          <h1>{recipe.title}</h1>
-          <p>{recipe.description}</p>
-          <EditRecipeForm recipe={recipe} />
-          <DeleteRecipeButton recipeId={recipe.id} />
-        </>
-      ) : (
-        <p>Recipe not found</p>
-      )}
+      <h1>{recipe.title}</h1>
+      <p>{recipe.description}</p>
+      {/* You can also add buttons here to edit or delete */}
     </div>
   );
 };
