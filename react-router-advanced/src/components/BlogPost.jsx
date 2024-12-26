9251f503b80f7bab2b45f07ea04ef09c5eb39741
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-27 01:31:00 +0200

    Create BlogPost.jsx

---
+++ react-router-advanced/src/components/BlogPost.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const BlogPost = () => {
+  const { postId } = useParams();
+
+  return (
+    <div>
+      <h1>Blog Post</h1>
+      <p>Post ID: {postId}</p>
+    </div>
+  );
+};
+
+export default BlogPost;
