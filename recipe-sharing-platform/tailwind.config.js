ba73b83263f99783d1e78211f77bf0dd5f5ba7ec
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-27 07:24:10 +0200

    Create tailwind.config.js

---
+++ recipe-sharing-platform/tailwind.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  darkMode: false, // Optional: 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
