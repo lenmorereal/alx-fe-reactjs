f4ca1e15df08dc40c330ead41fbb934889b5a4cc
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-27 08:04:45 +0200

    Update App.jsx

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
-import SearchUser from './components/SearchUser';
+import Search from './components/Search';
 
-function App() {
+const App = () => {
   return (
-    <div>
-      <SearchUser />
+    <div className="App">
+      <h1 className="text-center text-3xl font-bold p-6">GitHub User Search</h1>
+      <Search />
     </div>
   );
-}
+};
 
 export default App;
