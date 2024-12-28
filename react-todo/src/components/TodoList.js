7be97b326318d43d80232cdfd19da7ed22d9bd08
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2024-12-28 16:00:29 +0200

    Create  TodoList.js

---
+++ react-todo/src/components/TodoList.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+function TodoList() {
+  const [todos, setTodos] = useState([
+    { id: 1, text: 'Learn React', completed: false },
+    { id: 2, text: 'Build a Todo App', completed: false }
+  ]);
+
+  const addTodo = (text) => {
+    const newTodo = {
+      id: todos.length + 1,
+      text: text,
+      completed: false
+    };
+    setTodos([...todos, newTodo]);
+  };
+
+  const toggleTodo = (id) => {
+    setTodos(todos.map(todo =>
+      todo.id === id ? { ...todo, completed: !todo.completed } : todo
+    ));
+  };
+
+  const deleteTodo = (id) => {
+    setTodos(todos.filter(todo => todo.id !== id));
+  };
+
+  return (
+    <div>
+      <ul>
+        {todos.map(todo => (
+          <li key={todo.id}>
+            <span
+              onClick={() => toggleTodo(todo.id)}
+              style={{ textDecoration: todo.completed ? 'line-through' : 'none' }}
+            >
+              {todo.text}
+            </span>
+            <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+          </li>
+        ))}
+      </ul>
+      <button onClick={() => addTodo('New Todo')}>Add Todo</button>
+    </div>
+  );
+}
+
+export default TodoList;
