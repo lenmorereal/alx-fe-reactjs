0cc502137474cb893b6d090821e0ff5abf666af7
lenmorelms <72908654+lenmorereal@users.noreply.github.com>
2025-01-16 00:15:30 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,18 +1,16 @@
 // src/App.jsx
 import React from 'react';
-import { UserProvider } from './UserContext'; // Import the UserProvider component
-import UserProfile from './components/UserProfile'; // Import the UserProfile component
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
 
 function App() {
-    return (
-        // Wrap the app with UserProvider to provide the context
-        <UserProvider>
-            <div className="App">
-                <h1>Welcome to My React App</h1>
-                <UserProfile />
-            </div>
-        </UserProvider>
-    );
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return (
+    <UserContext.Provider value={userData}>
+      <ProfilePage />
+    </UserContext.Provider>
+  );
 }
 
 export default App;
